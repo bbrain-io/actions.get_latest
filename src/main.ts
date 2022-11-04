9fe77b7a6160ac28ce068a25c001a7cbe60af3a4
Jonathan Scherrer <jonathan.s.scherrer@gmail.com>
2022-11-04 18:45:18 +0100

    first version

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,64 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {getOctokit} from '@actions/github'
+import {GitHub} from '@actions/github/lib/utils'
+import * as semver from 'semver'
+
+
+async function getLatestReleaseByDate(client: InstanceType<typeof GitHub>, owner: string, repo: string) {
+  let release = await client.rest.repos.getLatestRelease({
+    owner: owner,
+    repo: repo
+  })
+  return release.data.tag_name
+}
+
+async function getLatestReleaseBySort(client: InstanceType<typeof GitHub>, owner: string, repo: string) {
+  var releases = await client.rest.repos.listReleases({
+    owner: owner,
+    repo: repo,
+    per_page: 100
+  });
+
+  var tags = []
+  for (const release of releases.data) {
+    let tag = release.tag_name
+    if (!semver.valid(tag)) {
+      continue
+    }
+    tags.push(tag)
+  }
+  tags.sort(semver.rcompare)
+  return tags[0]
+}
+
+function stringToBool(string: string): Boolean {
+  if (string.toLowerCase() === "true") {
+    return true
+  } else if (string.toLowerCase() === "false") {
+    return false
+  }
+  throw new Error("boolean type accepts the following values : [Ff]alse, [Tt]rue")
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('github-token', {required: true})
+    const repo: string = core.getInput('repo')
+    const owner: string = core.getInput('owner')
+    const by_date: Boolean = stringToBool(core.getInput('by_date'))
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const github = getOctokit(token)
 
-    core.setOutput('time', new Date().toTimeString())
+    if (by_date) {
+      var tag = await getLatestReleaseByDate(github, owner, repo)
+    } else {
+      var tag = await getLatestReleaseBySort(github, owner, repo)
+    }
+
+    core.setOutput('raw', tag)
+    core.setOutput('clean', semver.clean(tag))
+    core.setOutput('no_release', tag.split("-")[0])
+    core.setOutput('clean_no_release', semver.clean(tag)?.split("-")[0])
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
